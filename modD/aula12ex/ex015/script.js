54aa81986f85b86e9ed267449101ab73fcd3d9d3
Antonio Bezerra <89617448+profantoniobezerra@users.noreply.github.com>
2022-05-05 16:14:02 -0300

    atualizar

--- modD/aula12ex/ex015/script.js
+++ modD/aula12ex/ex015/script.js
@@ -9,6 +9,43 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //crianca
+                img.setAttribute('src','criancamas.png')
+            } else if (idade <21) {
+                //jovem
+                img.setAttribute('src','adoslmas.png')
+            } else if (idade <50) {
+                //adulto
+                img.setAttribute('src','adultomas.png')
+            } else {
+                //idoso
+                img.setAttribute('src','idosomas.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //crianca
+                img.setAttribute('src','criancafem.png')
+            } else if (idade <21) {
+                //jovem
+                img.setAttribute('src','adoslfem.png')
+            } else if (idade <50) {
+                //adulto
+                img.setAttribute('src','adultofem.png')
+            } else {
+                //idoso
+                img.setAttribute('src','idosafem.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
     
 }
